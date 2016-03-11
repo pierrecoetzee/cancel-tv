454317c9a8eb069a2083536101934db8b16c209e
Pierre Coetzee <pierre.coetzee@gmail.com>
2016-03-11 13:25:09 +0000

    this is a new commit with some changes ot the file load on start

--- lib/cancel_tv.rb
+++ lib/cancel_tv.rb
@@ -1,8 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__), 'cancel-tv')
 
 require 'ext'
+require 'cancel-tv/controllers'
+require 'cancel-tv/models'
 require 'sinatra/base'
 require 'yaml'
 require 'active_support'
 require 'active_support/core_ext'
-require 'controllers'
